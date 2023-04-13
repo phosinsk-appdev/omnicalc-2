dec754d145d2adbe11d8e5cfc9e0a0151af699d2
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-04-13 15:25:20 +0000

    add + subtract done

---
+++ app/controllers/math_controller.rb
@@ -0,0 +1,35 @@
+class MathController < ApplicationController
+
+  def add_results
+    
+    @first_number = params.fetch("first_num").to_f
+    @second_number = params.fetch("second_num").to_f
+    @result = @first_number + @second_number
+
+    render({:template => "math_templates/add_results.html.erb"})
+
+  end
+
+  def add_form
+
+    render({:template => "math_templates/add_form.html.erb"})
+
+  end
+
+  def subtract_results
+    
+    @first_number = params.fetch("first_num").to_f
+    @second_number = params.fetch("second_num").to_f
+    @result = @second_number - @first_number
+
+    render({:template => "math_templates/subtract_results.html.erb"})
+
+  end
+
+  def subtract_form
+
+    render({:template => "math_templates/subtract_form.html.erb"})
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+get("add", {:controller => "math", :action => "add_form"})
+get("wizard_add", {:controller => "math", :action => "add_results"})
+
+get("subtract", {:controller => "math", :action => "subtract_form"})
+get("wizard_subtract", {:controller => "math", :action => "subtract_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
