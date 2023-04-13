d2812b0e13381003776c949a629f512f1040f79a
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-04-13 15:33:47 +0000

    DONE

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -32,4 +32,36 @@ class MathController < ApplicationController
 
   end
 
+  def multiply_results
+    
+    @first_number = params.fetch("first_num").to_f
+    @second_number = params.fetch("second_num").to_f
+    @result = @second_number * @first_number
+
+    render({:template => "math_templates/multiply_results.html.erb"})
+
+  end
+
+  def multiply_form
+
+    render({:template => "math_templates/multiply_form.html.erb"})
+
+  end
+
+  def divide_results
+    
+    @first_number = params.fetch("first_num").to_f
+    @second_number = params.fetch("second_num").to_f
+    @result = @first_number / @second_number
+
+    render({:template => "math_templates/divide_results.html.erb"})
+
+  end
+
+  def divide_form
+
+    render({:template => "math_templates/divide_form.html.erb"})
+
+  end
+
 end
